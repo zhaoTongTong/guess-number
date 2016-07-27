66ed58fddf7a41958fd880b6e164c45ca0265d04
Tong_style <1141632973@qq.com>
2016-07-27 08:23:01 +0800

    build guess spec

--- spec/guess-spec.js
+++ spec/guess-spec.js
@@ -1,3 +1,12 @@
-class Guess {
-  
-}
+'use strict'
+
+const Guess = require('../src/models/guess');
+const answerGenerator = require('../src/models/random-number');
+
+describe('guess', () => {
+  it('should guess', () => {
+    spyOn(answerGenerator, 'randomNumber').and.returnValue('1234');
+    const result = Guess.guess('1234');
+    expect(result).toEqual('4A0B');
+  });
+});
