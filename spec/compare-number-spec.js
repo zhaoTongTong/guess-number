0da2eec74b1560ac1a6f73d577f830321dd685df
Tong_style <1141632973@qq.com>
2016-07-27 10:42:24 +0800

    compare number update

--- spec/compare-number-spec.js
+++ spec/compare-number-spec.js
@@ -2,14 +2,46 @@
 
 const answer = require('../src/models/compareNumber');
 
-describe('return text of', () => {
-
-  it('right answer', () => {
-    const defaultAnswer = '1234';
-    const inputNum = '1234';
-    const expectString = '4A0B';
-    const result = answer.comparedResult(defaultAnswer, inputNum);
-
-    expect(result).toEqual(expectString);
+// describe('return text of', () => {
+//
+//   it('right answer', () => {
+//     const defaultAnswer = '1234';
+//     const inputNum = '1234';
+//     const expectString = '4A0B';
+//     const result = answer.comparedResult(defaultAnswer, inputNum);
+//
+//     expect(result).toEqual(expectString);
+//   });
+// });
+describe('Compare Number', () => {
+  it('returns xAxB', () => {
+    [{
+      input: '5678',
+      answer: '1234',
+      result: '0A0B'
+    }, {
+      input: '1234',
+      answer: '1234',
+      result: '4A0B'
+    }, {
+      input: '1243',
+      answer: '1234',
+      result: '2A2B'
+    }, {
+      input: '5643',
+      answer: '1234',
+      result: '0A2B'
+    }, {
+      input: '4321',
+      answer: '1234',
+      result: '0A4B'
+    }, {
+      input: '1253',
+      answer: '1234',
+      result: '2A1B'
+    }].forEach(example => {
+      const result = answer.comparedResult(example.input, example.answer);
+      expect(result).toEqual(example.result);
+    })
   });
 });
--- src/models/compareNumber.js
+++ src/models/compareNumber.js
@@ -1,16 +1,14 @@
 
 class CompareNumber {
   static comparedResult(answer, inputNums) {
-    const numberOfB = inputNums.split('').map((item, index) => {
-      return (answer[index] !== item && answer.split('')
-          .find(element => element === item)? 1 : 0);
-    }).reduce((a, b) => a+b);
+    const inputs = inputNums.split('');
+    const answers = answer.split('');
 
-    const numberOfA = inputNums.split('').map((item, index, array) => {
-      return array.lastIndexOf(item) === index ? 1 : 0;
-    }).reduce((a, b) => a+b);
+    const rightDigitCount = inputs.filter(c => answers.includes(c)).length;
+    const x = inputs.filter(c => answers.indexOf(c) === inputs.indexOf(c)).length;
+    const y = rightDigitCount - x;
 
-    return `${numberOfA}A${numberOfB}B`;
+    return `${x}A${y}B`;
   }
 }
 
