e4801f8b067ff79472858e8e8ca5b93aa9f3099d
Tong_style <1141632973@qq.com>
2016-07-27 09:10:16 +0800

    complete play

--- src/play.js
+++ src/play.js
@@ -1,21 +1,34 @@
-// 'use strict'
-// const answer = require('./models/CompareNumber');
-// const number = require('./models/random-number');
-//
-// function getAnswer(defaultAnswer, inputNums) {
-//   return answer.comparedResult(defaultAnswer, inputNums);
-// }
-//
-// function getRandomNum() {
-//   return number.randomNumber();
-// }
-// function play(randomNum) {
-//
-//
-// }
-//
-// module.exports = {
-//   getAnswer:getAnswer,
-//   getRandomNum: getRandomNum,
-//   play: play
-// }
+'use strict'
+
+const scanf = require('scanf');
+const AnswerGenerator = require('./models/random-number');
+const compareNum = require('./models/compareNumber');
+
+function play() {
+  const isUnique = (item, index, array) => {
+    return array.lastIndexOf(item) === index;
+  }
+
+  console.log("welcome!\n");
+  const defaultNum = AnswerGenerator.randomNumber();
+  for(let times = 6; times > 0; times--) {
+    console.log(`Please input your number(${times})`);
+    const guessNum = scanf('%s');
+    if( !guessNum.split('').every(isUnique)) {
+      console.log('Cannot input duplicate numbers!');
+    }else {
+      const result = compareNum.comparedResult(guessNum, defaultNum);
+      if(result === '4A0B') {
+        console.log('Congratulations!');
+        return ;
+      }else {
+        console.log(result + '\n');
+      }
+    }
+
+  }
+
+  console.log('Game Over');
+}
+
+play();
