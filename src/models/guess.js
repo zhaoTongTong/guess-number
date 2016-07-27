3b660db58451c8ae31c3f394c42556f03fc7e203
Tong_style <1141632973@qq.com>
2016-07-27 08:22:23 +0800

    create guess class

--- src/models/guess.js
+++ src/models/guess.js
@@ -1,3 +1,14 @@
-/**
- * Created by ztt on 16-7-27.
- */
+'use strict'
+
+const randomNum = require('./random-number');
+const compareNum = require('./compareNumber');
+
+class Guess {
+  static guess(guessNum) {
+    const result = randomNum.randomNumber();
+    return compareNum.comparedResult(result, guessNum);
+  }
+
+}
+
+module.exports= Guess;
